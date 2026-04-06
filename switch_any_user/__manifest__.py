b786044aed886a65b0a91d0527d4217cc4f2c178
MausamGr <103228794+mausamgurung1@users.noreply.github.com>
2026-04-06 09:54:58 +0545

    Update __manifest__.py

--- switch_any_user/__manifest__.py
+++ switch_any_user/__manifest__.py
@@ -7,7 +7,7 @@
     'summary': 'Systray user switcher icon only visible when current user has permission',
     'author': 'Mausam Gurung',
     'currency': 'USD',
-    'price': '19.99',
+    'price': '4.99',
     'depends': ['base', 'web', 'mail'],
     'data': [
         'views/view.xml',
